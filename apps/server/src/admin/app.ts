f6e0684b2b50625aeb4f09dd6b1ef393ebbf352f
Per4atku <artcchi@gmail.com>
2025-11-12 22:05:00 +1000

    feature: admin panel translated

---
+++ apps/server/src/admin/app.ts
@@ -0,0 +1,56 @@
+import type { StrapiApp } from "@strapi/strapi/admin";
+
+export default {
+  config: {
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      "ru",
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+    translations: {
+      ru: {
+        // Общие элементы интерфейса
+        "Auth.form.email.label": "Электронная почта",
+        "Auth.form.password.label": "Пароль",
+        "Auth.form.button.login": "Войти",
+        "Auth.form.button.forgot-password": "Забыли пароль?",
+
+        // Навигация / меню
+        "content-manager.listView.header.title": "Записи",
+        "content-manager.createView.header.title": "Создать запись",
+        "content-manager.editView.header.title": "Редактировать запись",
+        "Settings.profile.form.section.experience.interfaceLanguage":
+          "Язык интерфейса",
+
+        "product.product/create": "Создать новый товар",
+      },
+    },
+  },
+
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
+};
